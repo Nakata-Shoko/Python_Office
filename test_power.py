c923843133d4231e37a8e416a0b033777de9b354
Nakata-Shoko <syouko0207@icloud.com>
2023-05-20 17:47:08 +0900

    commit

--- test_power.py
+++ test_power.py
@@ -1,12 +1,24 @@
 from pptx import Presentation
+import collections.abc
 
 prs = Presentation()
-title_slide_layout = prs.slide_layouts[0]
+title_slide_layout = prs.slide_layouts[1]
 slide = prs.slides.add_slide(title_slide_layout)
 title = slide.shapes.title
 subtitle = slide.placeholders[1]
 
-title.text = "Hello, World!"
-subtitle.text = "python-pptx was here!"
+title.text = "nice to meet you!"
+subtitle.text = "Welcome!"
 
-prs.save('test.pptx')
+count = 0
+for slide in prs.slides:
+    for shape in slide.shapes:
+        if not shape.has_text_frame:
+            continue
+        for paragraph in shape.text_frame.paragraphs:
+            for run in paragraph.runs:
+                count += len(run.text)
+
+print(f'パワーポイント内の文字数は{count}個です。')
+
+prs.save('test2.pptx')
--- test_word_img.py
+++ test_word_img.py
@@ -6,4 +6,11 @@ doc = Document("sample.docx")
 p1 = doc.add_paragraph('画像1を挿入します。')
 doc.add_picture('./picturefile/pict1.jpg', width=Mm(50))
 
+count = 0
+for para in doc.paragraphs:
+    count += len(para.text)
+print("このページの文字数は",count)
+
 doc.save("Sample.docx")
+
+doc.save("sample_answer.docx")
