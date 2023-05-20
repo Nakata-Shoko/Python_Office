cc60be668cda77776b79e6681907f84bfa5d94ad
Nakata-Shoko <syouko0207@icloud.com>
2023-05-20 16:40:47 +0900

    commit

--- test_excel.py
+++ test_excel.py
@@ -17,10 +17,11 @@ ws['A2'] = datetime.datetime.now()
 
 #ワークシートを追加
 ws1 = wb.create_sheet(title="Mysheet") 
-
-for x in range(1,101):
-    for y in range(1,101):
-        print(ws1.cell(row=x, column=y))
+data = 0
+for x in range(1,11):
+    for y in range(1,11):
+        date = 10 * (x - 1) + y
+        ws1.cell(row=x, column=y).value = date
 
 #シート名の確認
 print( wb.sheetnames )
